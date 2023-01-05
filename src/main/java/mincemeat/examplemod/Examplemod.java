6b37c343ede566bd2856f50251f05e9c279382b0
Christopher Maldonado <cmaldonadogomez@nevada.unr.edu>
2023-01-04 16:54:59 -0800

    testing

---
+++ src/main/java/mincemeat/examplemod/Examplemod.java
@@ -0,0 +1,10 @@
+package mincemeat.examplemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Examplemod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
